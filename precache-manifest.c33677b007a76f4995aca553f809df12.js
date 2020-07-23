49fd2f9e62ebc25f24279a8b89c48129dc5ebc3b
Isaac Sokari <isaacsokari@gmail.com>
2020-07-23 11:12:26 +0100

    Updates

---
+++ precache-manifest.c33677b007a76f4995aca553f809df12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fd23dbfbb948a9ff3c31fa745dcd90",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1abbdf83f5cc5cca47ab",
+    "url": "/monsters-rolodex/static/css/main.0f0c294b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1abbdf83f5cc5cca47ab",
+    "url": "/monsters-rolodex/static/js/main.8758e966.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
